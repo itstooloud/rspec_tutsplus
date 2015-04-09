bd3bdc68f09c00f9950084df6409da11a0837f80
Chris Collingwood <itstooloud@gmail.com>
2015-04-09 00:21:24 -0400

    RSpec, also yaml data dump

---
+++ book.rb
@@ -0,0 +1,12 @@
+class Book
+
+	attr_accessor :title, :author, :category
+
+	def initialize (title, author, category)
+		@title = title
+		@author = author
+		@category = category
+		
+		
+	end
+end
